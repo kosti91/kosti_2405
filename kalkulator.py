adb02b845fc8f892b82f9f6beb984dedb11d54f3
Kosti <kosti@wsdasd.pl>
2015-05-24 13:15:05 +0200

    funkcja get_info gotowa do testowania

--- kalkulator.py
+++ kalkulator.py
@@ -1,8 +1,13 @@
 def dodawanie():
-	print('Podaj pierwsza liczbe')
-	a = int(input())
-	print("Podaj druga liczbe")
-	b = int(input())
-	print(a+b)
+	return(a+b)
+print('Podaj pierwsza liczbe')
+a = int(input())
+print("Podaj druga liczbe")
+b = int(input())
 
+def get_info():
+	print("witaj w programie kalkulator")
+
+get_info()
 dodawanie()
+
